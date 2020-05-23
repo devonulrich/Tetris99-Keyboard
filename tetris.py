97c8686a9901be1f4a8ca2b1f2d82360f1a0cc39
Devon Ulrich <dpulrich@gmail.com>
2020-05-23 18:17:31 -0400

    Cleaned up files & made tetris.py more modular
    The keyboard variables in tetris.py are now stored and modified
    in a KeyboardState object, which avoids the need for global variables
    and helps keep the keyboard code more organized.

--- tetris.py
+++ tetris.py
@@ -11,75 +11,76 @@ from joycontrol.protocol import controller_protocol_factory
 from joycontrol.server import create_hid_server
 from joycontrol.memory import FlashMemory
 
-# keyboard order: up, down, left, right, space, c, enter
-keys = {
-    'a': False,
-    'down': False,
-    'left': False,
-    'right': False,
-    'up': False,
-    'r': False,
-    'l': False
-}
+class KeyboardState():
+    def __init__(self):
+        # key order: up, down, left, right, space, c, enter
+        self.keys = {
+			'a': False,
+			'down': False,
+			'left': False,
+			'right': False,
+			'up': False,
+			'r': False,
+			'l': False
+		}
+        self.direction = 0
 
-direction = 0
+    def key_down(self, key):
+        global direction
+        if key == Key.up:
+            self.keys['a'] = True
+        elif key == Key.down:
+            self.keys['down'] = True
+        elif key == Key.left:
+            self.keys['left'] = True
+        elif key == Key.right:
+            self.keys['right'] = True
+        elif key == Key.space:
+            self.keys['up'] = True
+        elif key == KeyCode.from_char('c'):
+            self.keys['r'] = True
+        elif key == Key.enter:
+            # press both R and L
+            self.keys['r'] = True
+            self.keys['l'] = True
+        elif key == KeyCode.from_char('a'):
+            self.direction = (self.direction + 1) % 5
+        elif key == Key.esc:
+            # quit
+            return False
 
-def key_down(key):
-    global direction
-    if key == Key.up:
-        keys['a'] = True
-    elif key == Key.down:
-        keys['down'] = True
-    elif key == Key.left:
-        keys['left'] = True
-    elif key == Key.right:
-        keys['right'] = True
-    elif key == Key.space:
-        keys['up'] = True
-    elif key == KeyCode.from_char('c'):
-        keys['r'] = True
-    elif key == Key.enter:
-        # press both R and L
-        keys['r'] = True
-        keys['l'] = True
-    elif key == KeyCode.from_char('a'):
-        direction = (direction + 1) % 5
-    elif key == Key.esc:
-        # quit
-        return False
-
-def key_up(key):
-    if key == Key.up:
-        keys['a'] = False
-    elif key == Key.down:
-        keys['down'] = False
-    elif key == Key.left:
-        keys['left'] = False
-    elif key == Key.right:
-        keys['right'] = False
-    elif key == Key.space:
-        keys['up'] = False
-    elif key == KeyCode.from_char('c'):
-        keys['r'] = False
-    elif key == Key.enter:
-        keys['r'] = False
-        keys['l'] = False
+    def key_up(self, key):
+        if key == Key.up:
+            self.keys['a'] = False
+        elif key == Key.down:
+            self.keys['down'] = False
+        elif key == Key.left:
+            self.keys['left'] = False
+        elif key == Key.right:
+            self.keys['right'] = False
+        elif key == Key.space:
+            self.keys['up'] = False
+        elif key == KeyCode.from_char('c'):
+            self.keys['r'] = False
+        elif key == Key.enter:
+            self.keys['r'] = False
+            self.keys['l'] = False
 
 # adapted from button_push() in controller_state.py
-async def main_loop(controller_state):
+async def main_loop(controller_state, keyboard_state):
     button_state = controller_state.button_state
 
-    for key in keys:
-        button_state.set_button(key, pushed=keys[key])
+    for key in keyboard_state.keys:
+        button_state.set_button(key, pushed=keyboard_state.keys[key])
 
     stick_state = controller_state.r_stick_state
-    if direction == 0:
+    if keyboard_state.direction == 0:
         stick_state.set_center()
-    elif direction == 1:
+    elif keyboard_state.direction == 1:
         stick_state.set_right()
-    elif direction == 2:
+    elif keyboard_state.direction == 2:
         stick_state.set_down()
-    elif direction == 3:
+    elif keyboard_state.direction == 3:
         stick_state.set_left()
     else:
         stick_state.set_up()
@@ -101,9 +102,11 @@ async def _main(bt_addr):
     print("Connected.")
 
     # start key listener thread
+    keyboard_state = KeyboardState()
     listener = None
     try:
-        listener = Listener(on_press=key_down, on_release=key_up, suppress=True)
+        listener = Listener(on_press=keyboard_state.key_down, on_release=keyboard_state.key_up,
+            suppress=True)
     except:
         raise
     finally:
@@ -112,7 +115,7 @@ async def _main(bt_addr):
     # await button_push(controller_state, 'home', sec=5)
     pastTime = time.time()
     while listener.running:
-        await main_loop(controller_state)
+        await main_loop(controller_state, keyboard_state)
         # currTime = time.time()
         # print(currTime - pastTime)
         # pastTime = currTime
